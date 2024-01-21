9621f6955d4a0a2dee7e849574053a499919aae3
Andrew Lansangan <Andrew.J.Lansangan@gmail.com>
2024-01-21 15:58:48 -0500

    lol

--- express/src/app.ts
+++ express/src/app.ts
@@ -3,13 +3,14 @@ import cors from "cors";
 import express, { Express, Request, Response } from "express";
 import { google } from "googleapis";
 //import { authenticate } from "./middleware/githubClient";
+// import { App, createNodeMiddleware } from "octokit";
 import morgan from "morgan";
-import { authorize } from "./middleware/googleclient";
 import { router } from "./middleware/routes";
 import { setHeaders } from "./middleware/middlewares";
 import {readJsonFile} from "./middleware/readJsonFile";
-import { App, createNodeMiddleware } from "octokit";
-import {GoogleAuth} from "google-auth-library";
+import {GoogleAuth, OAuth2Client} from "google-auth-library";
+
+// import {credentials} from './middleware/googleOauth'
 //initialize Express
 const app: Express = express();
 const expressApp: Express = express();
@@ -24,12 +25,12 @@ interface MyObject {
 //   oauth: { clientId, clientSecret },
 // });
 
-expressApp.use(createNodeMiddleware(app));
+// expressApp.use(createNodeMiddleware(app));
 
-const octokit = {
-  auth: process.env.GITHUB_ACCESS_TOKEN,
-};
-app.use("/github_api", router);
+// const octokit = {
+//   auth: process.env.GITHUB_ACCESS_TOKEN,
+// };
+// app.use("/github_api", router);
 app.use(morgan("dev"));
 app.use(cors());
 app.use(express.json());
@@ -52,51 +53,78 @@ app.post("/api/contact", (req: Request, res: Response) => {
   res.json({ success: true });
 });
 
-app.get("/calendar", async (req: Request, res: Response) => {
-  try {
-    const auth = await authorize();
+// app.get("/calendar", async (req: Request, res: Response) => {
+//   try {
+//     // const auth = await authorize();
 
-    const calendar = google.calendar({ version: "v3", auth });
+//     const calendar = google.calendar({ version: "v3", auth });
 
-    // List upcoming events from the primary calendar
-    const events = await calendar.events.list({
-      calendarId: "primary",
-      timeMin: new Date().toISOString(),
-      maxResults: 10,
-      singleEvents: true,
-      orderBy: "startTime",
-    });
+//     // List upcoming events from the primary calendar
+//     const events = await calendar.events.list({
+//       calendarId: "primary",
+//       timeMin: new Date().toISOString(),
+//       maxResults: 10,
+//       singleEvents: true,
+//       orderBy: "startTime",
+//     });
 
-    const upcomingEvents = events.data.items;
-    res.json({ upcomingEvents });
-  } catch (error) {
-    console.error(error);
-    res.status(500).send("Internal Server Error");
-  }
-});
+//     const upcomingEvents = events.data.items;
+//     res.json({ upcomingEvents });
+//   } catch (error) {
+//     console.error(error);
+//     res.status(500).send("Internal Server Error");
+//   }
+// });
 
 async function main() {
   //await authenticate();
-  const filePath = 'credentials.json';
-  try {
-    const objectArray = await readJsonFile(filePath);
-    if (objectArray) {
-      console.log('Array of objects:', objectArray); // Log the content
-    } else {
-      console.log('Error reading or parsing JSON.');
-    }
-  } catch (error) {
-    console.error('Error:', error);
-  }
+  // const filePath = './credentials.json';
+  // try {
+  //   const objectArray = await readJsonFile(credentials);
+  //   if (objectArray) {
+  //     console.log('Array of objects:', objectArray); // Log the content
+  //   } else {
+  //     console.log('Error reading or parsing JSON.');
+  //   }
+  // } catch (error) {
+  //   console.error('Error:', error);
+  // }
 
-  const auth = new GoogleAuth({
-    scopes: 'https://www.googleapis.com/auth/cloud-platform'
-  });
-  const client = await auth.getClient();
-  const projectId = await auth.getProjectId();
-  const url = `https://dns.googleapis.com/dns/v1/projects/${projectId}`;
-  const res = await client.request({ url });
-  console.log(res.data);
+//   const auth = new GoogleAuth({
+//     scopes: 'https://www.googleapis.com/auth/cloud-platform'
+//   });
+//   const client = await auth.getClient();
+//   const projectId = await auth.getProjectId();
+//   const url = `https://dns.googleapis.com/dns/v1/projects/${projectId}`;
+//   const res = await client.request({ url });
+//   console.log(res.data);
+// }
+app.get('/oauth2callback', async (req, res) => {
+  const code = req.query.code as string;
+  console.log(code)
+  const clientId =
+    "493869205289-084nogbgd8dthlqjrt1umf56fbco51i5.apps.googleusercontent.com";
+  const clientSecret = "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";
+  const redirectUri = "http://localhost:3000/";
+   
+  const oAuth2Client = new OAuth2Client(
+    clientId,
+    clientSecret,
+    redirectUri
+  );
+
+  try {
+    const tokenResponse = await oAuth2Client.getToken(code);
+    const accessToken = tokenResponse.tokens.access_token;
+
+    console.log('Access Token:', accessToken);
+    res.send('Authentication successful! You can close this tab now.');
+  } catch (error) {
+    console.error('Error getting token:', error);
+    res.status(500).send('Error during authentication.');
+  }
+});
+
 }
 
 main().catch(err => console.error(err));
@@ -104,8 +132,9 @@ const port = process.env.PORT || 3000;
 const server = app.listen(port, () => {
   console.log(`Listening at http://localhost:${port}/api`);
 });
-expressApp.listen(3001, () => {
-  console.log(`Listening at http://localhost:3001/api`);
-});
+// expressApp.listen(3001, () => {
+//   console.log(`Listening at http://localhost:3001/api`);
+// });
 
-server.on("error", console.error);
+server.on("error", console.error)
+
--- express/src/middleware/appWebserver.ts
+++ express/src/middleware/appWebserver.ts
@@ -1,32 +1,32 @@
-import url from "url";
-async function createNodeMiddleware() {
-  const code = new URL(location.href).searchParams.get("code");
-  if (code) {
-    // remove ?code=... from URL
-    const path =
-      location.pathname +
-      location.search.replace(/\b(code|state)=\w+/g, "").replace(/[?&]+$/, "");
-    history.replaceState({}, "", path);
+// import url from "url";
+// async function createNodeMiddleware() {
+//   const code = new URL(location.href).searchParams.get("code");
+//   if (code) {
+//     // remove ?code=... from URL
+//     const path =
+//       location.pathname +
+//       location.search.replace(/\b(code|state)=\w+/g, "").replace(/[?&]+$/, "");
+//     history.replaceState({}, "", path);
 
-    // exchange the code for a token with your backend.
-    // If you use https://github.com/octokit/oauth-app.js
-    // the exchange would look something like this
-    const response = await fetch("/api/github/oauth/token", {
-      method: "POST",
-      headers: {
-        "content-type": "application/json",
-      },
-      body: JSON.stringify({ code }),
-    });
-    const { token } = await response.json();
-    // `token` is the OAuth Access Token that can be use
+//     // exchange the code for a token with your backend.
+//     // If you use https://github.com/octokit/oauth-app.js
+//     // the exchange would look something like this
+//     const response = await fetch("/api/github/oauth/token", {
+//       method: "POST",
+//       headers: {
+//         "content-type": "application/json",
+//       },
+//       body: JSON.stringify({ code }),
+//     });
+//     const { token } = await response.json();
+//     // `token` is the OAuth Access Token that can be use
 
-    const { Octokit } = await import("https://esm.sh/@octokit/core");
-    const octokit = new Octokit({ auth: token });
+//     const { Octokit } = await import("https://esm.sh/@octokit/core");
+//     const octokit = new Octokit({ auth: token });
 
-    const {
-      data: { login },
-    } = await octokit.request("GET /user");
-    alert("Hi there, " + login);
-  }
-}
+//     const {
+//       data: { login },
+//     } = await octokit.request("GET /user");
+//     alert("Hi there, " + login);
+//   }
+// }
